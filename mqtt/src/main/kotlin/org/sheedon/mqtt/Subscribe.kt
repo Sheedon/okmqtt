8808bb7864e5d1641ee60b134161f7d3c81e6645
sheedon <sheedonsun@163.com>
2022-03-10 21:52:31 +0800

    补充Subscribe订阅类，作为订阅的Request

--- mqtt/src/main/kotlin/org/sheedon/mqtt/Subscribe.kt
+++ mqtt/src/main/kotlin/org/sheedon/mqtt/Subscribe.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Sheedon.
+ * Copyright (C) 2020 Sheedon.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,27 +27,39 @@ import androidx.annotation.IntRange
 class Subscribe(
     @get:JvmName("topic") val topic: String,
     @get:JvmName("qos") val qos: Int = 0,
-    @get:JvmName("userContext") val userContext: Any? = null
+    @get:JvmName("userContext") val userContext: Any? = null,
+    @get:JvmName("attachRecord") val attachRecord: Boolean = false
 ) {
 
     fun convertKey(): String {
-        return topic + qos
+        return "$topic$$qos"
     }
 
     fun newBuilder(): Builder = Builder(this)
 
     open class Builder {
+        // 主题
         internal var topic: String? = null
+
+        // 质量
         internal var qos: Int
+
+        // 用户上下文
         internal var userContext: Any? = null
+
+        // 是否附加到缓存记录中，若false，则代表单次订阅，清空行为后，不恢复
+        internal var attachRecord: Boolean
 
         constructor() {
             qos = 0
+            attachRecord = false
         }
 
         internal constructor(subscribe: Subscribe) {
             this.topic = subscribe.topic
             this.qos = subscribe.qos
+            this.userContext = subscribe.userContext
+            this.attachRecord = subscribe.attachRecord
         }
 
         /**
@@ -74,19 +86,34 @@ class Subscribe(
             this.userContext = userContext
         }
 
+        /**
+         * Sets the attachRecord target of this subscribe.
+         */
+        open fun attachRecord(attachRecord: Boolean) = apply {
+            this.attachRecord = attachRecord
+        }
+
         open fun build(): Subscribe {
             return Subscribe(
                 checkNotNull(topic) { "topic == null" },
                 qos,
-                userContext
+                userContext,
+                attachRecord
             )
         }
     }
 
     companion object {
-        fun build(topic: String, qos: Int): Subscribe {
+        @JvmStatic
+        @JvmOverloads
+        fun build(
+            topic: String,
+            qos: Int,
+            attachRecord: Boolean = false
+        ): Subscribe {
             return Builder().topic(topic)
                 .qos(qos)
+                .attachRecord(attachRecord)
                 .build()
         }
     }
