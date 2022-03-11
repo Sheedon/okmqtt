3e1e7eedded37f717276afefa9baee7b3159eeb0
sheedon <sheedonsun@163.com>
2022-03-11 22:16:09 +0800

    订阅职责

--- mqtt/src/main/kotlin/org/sheedon/mqtt/Observable.kt
+++ mqtt/src/main/kotlin/org/sheedon/mqtt/Observable.kt
@@ -1,36 +1,44 @@
-/*
- * Copyright (C) 2022 Sheedon.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.sheedon.mqtt
 
-import org.sheedon.rr.core.Observable
+import org.sheedon.mqtt.listener.Callback
 
 /**
- * Observations are messages for which subscriptions are specified. The message can be cancelled.
- *
+ * 订阅
  * @Author: sheedon
  * @Email: sheedonsun@163.com
- * @Date: 2022/1/30 8:12 下午
+ * @Date: 2022/3/9 10:38 下午
  */
-interface Observable : Observable<String, RequestBody, ResponseBody> {
+interface Observable {
 
     /**
-     * Use the Callback in the protocol, you can also use the default subscribe (RRCallback callback),
-     * but the generic type is too much
+     * 订阅，指代于需要订阅的任务
      *
-     * @param callback Callback
+     * @param callback 反馈内容
      */
     fun subscribe(callback: Callback?)
+
+    /**
+     * 取消订阅，指代于需要取消订阅的任务
+     *
+     * @param callback 反馈内容
+     */
+    fun unsubscribe(callback: Callback?)
+
+
+    /**
+     * 获取订阅数据
+     *
+     * @return Subscribe 订阅信息
+     */
+    fun subscribes(): List<Subscribe>
+
+    /**
+     * 消息是否取消
+     */
+    fun isCanceled(): Boolean
+
+    /**
+     * 取消任务
+     */
+    fun cancel()
 }
