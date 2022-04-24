b2a9bea9b002dc1210c069d3ecdc2cca1b5dc78d
sheedon <sheedonsun@163.com>
2022-04-24 22:14:56 +0800

    添加关联类的注释

--- mqtt/src/main/kotlin/org/sheedon/mqtt/Relation.kt
+++ mqtt/src/main/kotlin/org/sheedon/mqtt/Relation.kt
@@ -3,17 +3,30 @@ package org.sheedon.mqtt
 import kotlin.jvm.Throws
 
 /**
- * 请求对象的关联者
- * 版本1，新增订阅主题集合
+ * The purpose is to associate a request object with key data that acts on the response.
+ *
+ * The associated information is one of [topics] and [keyword], If both are configured,
+ * limited use of [keyword].
+ *
+ * One is to configure the [topic],
+ * which extends the logic of MQTT to subscribe messages through topic,
+ * The subscription topic is consistent with the target topic, or the subscription topic uses wildcards,
+ * and the target topic is within the coverage of the wildcard, that is, the association is completed.
+ *
+ * Other is the configuration parameter [keyword]. Developers can add a keyword conversion
+ * strategy in [OkMqttClient.Builder.keywordConverter] as the keyword matching logic.
+ *
+ * And if [Relation.timeout] is not set, the global timeout is used by default.
+ * Otherwise, the timeout duration is set according to the custom timeout.
  *
  * @Author: sheedon
  * @Email: sheedonsun@163.com
  * @Date: 2022/3/7 10:52 下午
  */
 class Relation private constructor(
-    @get:JvmName("topics") val topics: Topics? = null,// 需要订阅的集合
-    @get:JvmName("keyword") val keyword: String? = null,// 订阅到关键字
-    @get:JvmName("timeout") val timeout: Long? = null,// 超时时长
+    @get:JvmName("topics") val topics: Topics? = null,
+    @get:JvmName("keyword") val keyword: String? = null,
+    @get:JvmName("timeout") val timeout: Long? = null,
 ) {
 
     fun newBuilder(): Builder {
@@ -37,27 +50,27 @@ class Relation private constructor(
             topics?.also {
                 append("{topic:${topics.topic}, qos:${topics.qos}}")
             }
+            keyword?.also {
+                append("{keyword:${keyword}}")
+            }
+            append(",{timeout:$timeout}")
+
         }
     }
 
     open class Builder {
-        // 订阅主题
         internal var topics: Topics? = null
-
-        // 订阅到关键字
         internal var keyword: String? = null
-
-        // 超时时长
         internal var timeout: Long? = null
 
-        // 是否添加绑定行为，订阅主题/关键字，当前版本只能二选一
+        // Whether to add binding behavior, subscribe topic keywords, the current version can only choose one
         internal var getSubscribe = false
         internal var getKeyWork = false
 
         /**
-         * 需要订阅mqtt主题
+         * Sets mqtt topic used to match the response topic.
          *
-         * @param topics mqtt主题
+         * @param topics mqtt topic configuration information
          * @return Builder
          */
         @Throws(IllegalStateException::class)
@@ -70,10 +83,12 @@ class Relation private constructor(
         }
 
         /**
-         * 订阅下的响应关键字
+         * Sets the keyword of the response message
          *
-         * @param keyword 关键字
-         * @return Builder
+         * If the field is not "", it means that the correlation field is used as the matching field
+         * of the response message for correlation; otherwise, the subscription topic in the relation is taken
+         *
+         * @param keyword the keyword of the response message
          */
         open fun keyword(keyword: String) = apply {
             this.getKeyWork = true
@@ -84,20 +99,22 @@ class Relation private constructor(
         }
 
         /**
-         * 单次请求超时额外设置
+         * Sets the request timeout value.
+         * This value, measured in millisecond,defines the maximum time interval
+         * the request will wait for the network callback to the MQTT Message response to be established.
          *
-         * @param delayMilliSecond 延迟时间（毫秒）
-         * @return Builder
+         * @param delayMilliSecond request timeout value (millisecond)
          */
         open fun delayMilliSecond(delayMilliSecond: Long) = apply {
             this.timeout = delayMilliSecond
         }
 
         /**
-         * 单次请求超时额外设置
+         * Sets the request timeout value.
+         * This value, measured in seconds,defines the maximum time interval
+         * the request will wait for the network callback to the MQTT Message response to be established.
          *
-         * @param delaySecond 延迟时间（秒）
-         * @return Builder
+         * @param delayMilliSecond request timeout value (seconds)
          */
         open fun delaySecond(delaySecond: Int) = apply {
             this.timeout = delaySecond * 1000L
